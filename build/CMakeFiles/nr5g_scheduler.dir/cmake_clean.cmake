b75c0579fa0d54a544956f5f9445c26e52d9a31d
KashibaiP <pkashibai94@gmail.com>
2026-07-04 12:11:57 +0000

    Restructure: organise files into src/ include/ tests/ folders

---
+++ build/CMakeFiles/nr5g_scheduler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nr5g_scheduler.dir/link.d"
+  "CMakeFiles/nr5g_scheduler.dir/src/main.cpp.o"
+  "CMakeFiles/nr5g_scheduler.dir/src/main.cpp.o.d"
+  "nr5g_scheduler"
+  "nr5g_scheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nr5g_scheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/nr5g_scheduler.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nr5g_scheduler.
+# This may be replaced when dependencies are built.
